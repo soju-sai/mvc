7b2a6b3319b783aa76a885c198cc875971f6b794
soju-sai <kiraarus@gmail.com>
2023-08-20 17:38:52 +0800

    11.1 Basic Authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: ENV["NAME"], password: ENV["PASSWORD"], except: [:index, :show]
+  
   def index
     @articles = Article.all
   end
